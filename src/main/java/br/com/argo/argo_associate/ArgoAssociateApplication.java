3e3e583e44939e2d5b706de5028ac113c6d1e8bd
Alex <alexgithub94@gmail.com>
2026-02-04 16:47:25 -0300

    initial

---
+++ src/main/java/br/com/argo/argo_associate/ArgoAssociateApplication.java
@@ -0,0 +1,13 @@
+package br.com.argo.argo_associate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgoAssociateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgoAssociateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/argo/argo_associate/ArgoAssociateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.argo.argo_associate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgoAssociateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
